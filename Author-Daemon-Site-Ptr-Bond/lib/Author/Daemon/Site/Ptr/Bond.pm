c0a4ef89a1d72e7f3d8e150badad86b290954972
Paul G Webster <paul.webster@opusvl.com>
2022-10-09 21:48:30 +0100

    Updated readme and created dzil package

---
+++ Author-Daemon-Site-Ptr-Bond/lib/Author/Daemon/Site/Ptr/Bond.pm
@@ -0,0 +1,24 @@
+#!/usr/bin/env perl
+
+# Perl core
+use v5.32;
+use strict;
+use warnings;
+use experimental "signatures";
+
+# Version
+our $VERSION = '0.01';
+
+package Author::Daemon::Site::Ptr::Bond;
+
+# Perl bundled
+use Carp qw(cluck longmess shortmess);
+
+# Private local
+
+# Private remote
+
+# Community
+
+
+1;
