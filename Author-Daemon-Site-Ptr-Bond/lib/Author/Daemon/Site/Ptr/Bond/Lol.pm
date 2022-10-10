b6eac5fb54644ed6ff00c389e8b315224d4ac64e
Paul G Webster <paul.webster@opusvl.com>
2022-10-10 21:26:37 +0100

    Added hard version tests

---
+++ Author-Daemon-Site-Ptr-Bond/lib/Author/Daemon/Site/Ptr/Bond/Lol.pm
@@ -0,0 +1,20 @@
+#!/usr/bin/env perl
+
+package Author::Daemon::Site::Ptr::Bond::Lol;
+
+# Perl core
+use v5.32;
+use strict;
+use warnings;
+use experimental "signatures";
+
+# Perl bundled
+use Carp qw(cluck longmess shortmess);
+
+# Private local
+
+# Private remote
+
+# Community
+
+1;
