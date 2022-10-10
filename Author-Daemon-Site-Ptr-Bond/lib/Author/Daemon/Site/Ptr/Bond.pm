2a0ed92213ca55eea206bd54e543e6fe2998f798
Paul G Webster <paul.webster@opusvl.com>
2022-10-10 21:19:23 +0100

    Bump version

--- Author-Daemon-Site-Ptr-Bond/lib/Author/Daemon/Site/Ptr/Bond.pm
+++ Author-Daemon-Site-Ptr-Bond/lib/Author/Daemon/Site/Ptr/Bond.pm
@@ -8,7 +8,7 @@ use experimental "signatures";
 
 # ABSTRACT: turns baubles into trinkets
 # Version
-our $VERSION = '0.006';
+our $VERSION = '0.008';
 
 # Perl bundled
 use Carp qw(cluck longmess shortmess);
