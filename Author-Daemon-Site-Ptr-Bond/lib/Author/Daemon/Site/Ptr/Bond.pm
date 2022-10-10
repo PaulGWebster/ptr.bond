17e1c40f191160bca26e35c4270aa2f5389886e6
Paul G Webster <paul.webster@opusvl.com>
2022-10-10 02:40:26 +0100

    Testing new dist.ini order

--- Author-Daemon-Site-Ptr-Bond/lib/Author/Daemon/Site/Ptr/Bond.pm
+++ Author-Daemon-Site-Ptr-Bond/lib/Author/Daemon/Site/Ptr/Bond.pm
@@ -8,7 +8,7 @@ use experimental "signatures";
 
 # ABSTRACT: turns baubles into trinkets
 # Version
-our $VERSION = '0.03';
+our $VERSION = '0.005';
 
 # Perl bundled
 use Carp qw(cluck longmess shortmess);
